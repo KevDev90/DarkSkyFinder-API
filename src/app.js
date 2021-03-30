506437df37d0509e5dbfc44d8eeeebe7cbd1e2a1
Kevin Johnson <kevinj902302@gmail.com>
2021-03-30 11:14:38 -0600

    Adds more boilerplate code

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const {CLIENT_ORIGIN} = require('./config');
 const { NODE_ENV } = require('./config')
 
 const app = express()
@@ -13,21 +14,27 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+  cors({
+      origin: CLIENT_ORIGIN
+  })
+);
+
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
-  } else {
-    console.error(error)
-    response = { message: error.message, error }
-  }
-  res.status(500).json(response)
-})
+
+  app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+
 
 module.exports = app
