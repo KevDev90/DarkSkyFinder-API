a46003dbdf615f55f7272404c23a348f68ed2d4f
Kevin Johnson <kevinj902302@gmail.com>
2021-04-08 11:22:50 -0600

    Adds ssl configuration for pg

--- src/server.js
+++ src/server.js
@@ -2,13 +2,22 @@ const express = require('express');
 const knex = require("knex");
 const app = require('./app')
 const { DATABASE_URL } = require("./config")
+const pg = require('pg');
+pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 
 const PORT = process.env.PORT || 8000;
 
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+    // connection: {
+  //   ssl: true,
+  //   host:'ec2-54-211-176-156.compute-1.amazonaws.com',
+  //   user:'shfqsekarskwby',
+  //   password:'9df5be1017edb015aada87fd385802c476ee8770569331f386d2cb49f9fa0624',
+  //   database:'db65fnmlmrmiob',
+  //   options:{encrypt:true}
+  //   }
 })
 
 app.set('db', db)
